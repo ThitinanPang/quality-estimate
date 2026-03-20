1b6f2cdd1cb59b44d0cfcfee1f790201c3656eee
Pang_Thitinan <65160217@go.buu.ac.th>
2026-03-20 17:30:17 +0700

    Update editassessor.blade.php

--- example-app/resources/views/editassessor.blade.php
+++ example-app/resources/views/editassessor.blade.php
@@ -4,35 +4,35 @@
     <div class="flex flex-col items-center justify-center mt-[32px]">
         <p class="text-[24px] w-[900px] h-[40px] bg-[#FFCE00] text-center border border-b-0 rounded-t-[14px]">
             แก้ไขข้อมูลผู้ประเมิน</p>
-        <form action="{{ route('updateassessor', $user->id) }}" method="POST">
+        <form action="{{ route('updateassessor', $userassessor->id) }}" method="POST">
             @csrf
             <div
                 class="border rounded-b-[39px] bg-[#DBDBDB] w-[900px] h-[780px] pl-[40px] pr-[40px] pt-[10px] overflow-y-auto">
                 <p class="mt-[9px]">ตำแหน่ง</p>
                 <select name="role" class="bg-white h-[25px] w-[800px] border rounded mt-[9px] pl-3">
-                    <option value="user" {{ $user->role == 'user' ? 'selected' : '' }}>user</option>
-                    <option value="admin" {{ $user->role == 'admin' ? 'selected' : '' }}>admin</option>
-                    <option value="admin university" {{ $user->role == 'admin university' ? 'selected' : '' }}>admin university</option>
+                    <option value="user" {{ $userassessor->role == 'user' ? 'selected' : '' }}>user</option>
+                    <option value="admin" {{ $userassessor->role == 'admin' ? 'selected' : '' }}>admin</option>
+                    <option value="admin university" {{ $userassessor->role == 'admin university' ? 'selected' : '' }}>admin university</option>
                 </select>
 
                 <p class="mt-[9px]">คำนำหน้า</p>
-                <input type="text" name="prefix" value="{{ $user->prefix }}"
+                <input type="text" name="prefix" value="{{ $userassessor->prefix }}"
                     class="bg-white h-[25px] w-[800px] border rounded mt-[9px] pl-3">
 
                 <p class="mt-[9px]">ชื่อ - สกุล</p>
-                <input type="text" name="name" value="{{ $user->name }}"
+                <input type="text" name="name" value="{{ $userassessor->name }}"
                     class="bg-white h-[25px] w-[800px] border rounded mt-[9px] pl-3">
 
                 <p class="mt-[9px]">กลุ่มวิชา</p>
-                <input type="text" name="subject_group" value="{{ $user->subject_group }}"
+                <input type="text" name="subject_group" value="{{ $userassessor->subject_group }}"
                     class="bg-white h-[25px] w-[800px] border rounded mt-[9px] pl-3">
 
                 <p class="mt-[9px]">คณะ</p>
-                <input type="text" name="faculty" value="{{ $user->faculty }}"
+                <input type="text" name="faculty" value="{{ $userassessor->faculty }}"
                     class="bg-white h-[25px] w-[800px] border rounded mt-[9px] pl-3">
 
                 <p class="mt-[9px]">หลักสูตร</p>
-                <input type="text" name="course" value="{{ $user->course }}"
+                <input type="text" name="course" value="{{ $userassessor->course }}"
                     class="bg-white h-[25px] w-[800px] border rounded mt-[9px] pl-3">
 
                 <p class="mt-[9px]">Assessor Type</p>
@@ -47,17 +47,17 @@
                     class="bg-white h-[25px] w-[800px] border rounded mt-[9px] pl-3">
 
                 <p class="mt-[9px]">อีเมล</p>
-                <input type="email" name="email" value="{{ $user->email }}"
+                <input type="email" name="email" value="{{ $userassessor->email }}"
                     class="bg-white h-[25px] w-[800px] border rounded mt-[9px] pl-3">
 
                 <p class="mt-[9px]">เบอร์โทรศัพท์</p>
-                <input type="text" name="phone_number" value="{{ $user->phone_number }}"
+                <input type="text" name="phone_number" value="{{ $userassessor->phone_number }}"
                     class="bg-white h-[25px] w-[800px] border rounded mt-[9px] pl-3">
 
                 <p class="mt-[9px]">status</p>
                 <select name="status" class="bg-white h-[25px] w-[800px] border rounded mt-[9px] pl-3">
-                    <option value="active" {{ $user->status == 'active' ? 'selected' : '' }}>active</option>
-                    <option value="inactive" {{ $user->status == 'inactive' ? 'selected' : '' }}>inactive</option>
+                    <option value="active" {{ $userassessor->status == 'active' ? 'selected' : '' }}>active</option>
+                    <option value="inactive" {{ $userassessor->status == 'inactive' ? 'selected' : '' }}>inactive</option>
                 </select>
             </div>
             <div class=" justify-center flex gap-[50px]">
